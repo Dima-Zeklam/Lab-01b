ba3578ef26a0e19025439b6a45848e508d08b49c
Dima Zeklam <dema.ziklam@gmail.com>
2021-06-20 15:24:00 +0300

    new page

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+let firstName = prompt("What is your first name ?");
+let lastName = prompt("What is you last name ?");
+let favFood = prompt("What is your favorit food ?");
+let favColor = prompt("What is your favorit color ?");
+
+alert("Hi " + firstName +' '+ lastName + ", " + favFood + 
+" is a delicious dish and " + favColor + " is a beautiful color" );
+
+console.log("What is your first name ? " + firstName);
+console.log("What is you last name ? " + lastName);
+console.log("What is your favorit food ? " + favFood);
+console.log("What is your favorit color ? " + favColor);
